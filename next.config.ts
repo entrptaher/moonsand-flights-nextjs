a7f74034eaea631d266c35d4ecdfd1f77bb3ddcf
entrptaher@gmail.com <entrptaher@gmail.com>
2025-08-05 14:00:36 +0600

    fix: pics.avs.io

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'pics.avs.io',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
